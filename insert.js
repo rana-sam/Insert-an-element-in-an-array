1a96d60e43d03a480e651e6463fb057e9ef8c422
Muhammad Usama <82697108+rana-sam@users.noreply.github.com>
2021-04-24 15:52:43 +0500

    Done

--- insert.js
+++ insert.js
@@ -1,16 +1,12 @@
-let array=[12,22,3,45,2,67,21]
-let place=parseInt(prompt("press 1 to insert at end.    press 2 to insert at start."))
-let num=parseInt(prompt("Enter element to isert"))
+let array=[12,23,6,44,96,09]
+let place=parseInt(prompt("Enter Index where you wants to enter an element."))
+let num=parseInt(prompt("Enter number to place"))
 switch (place) {
-    case 1:
-        array.push(num)
+    case place:
+        array.splice(place,0,num)
         console.log(array);
         break;
-    case 2:
-         array.unshift(num)
-         console.log(array);
-         break;
+
     default:
         break;
-    }
-    
+}
